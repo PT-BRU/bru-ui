54668369dc9b49e15b7d45b4de64c4a8ea94e890
achensetyawati <achenzetya@gmail.com>
2024-01-29 09:49:15 +0700

    add size input

--- src/modules/laporan/sales/report.js
+++ src/modules/laporan/sales/report.js
@@ -118,7 +118,7 @@ export class List {
         this.error = {};
         if (Object.getOwnPropertyNames(this.error).length === 0) {
             this.flag = true;
-            this.Table.refresh();
+            //this.Table.refresh();
         }
     }
 
